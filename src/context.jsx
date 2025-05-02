e708d3b8a32220aa0c12da1344229b6c05b8c6d8
Nurasyl <nnurasil78@gmail.com>
2025-05-02 20:00:09 +0500

    first commit

---
+++ src/context.jsx
@@ -0,0 +1,39 @@
+import { useContext, useState, createContext } from 'react'
+
+const AppContext = createContext()
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSideBarOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSideBarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSideBarOpen(false)
+  }
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
